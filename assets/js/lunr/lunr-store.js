a52c73b6f4dbfee9c002cca9db96fea092af17c8
SV-97 <volzstefan97@googlemail.com>
2021-07-27 12:32:35 +0200

    Testytest

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "McCulloch/Pitts neuron model",
+        "excerpt":"So I came across the McCulloch/Pitts (I’ll just call it Mcp) neuron model while reading on ANNs today. The Mcp is a comparatively easy model that doesn’t have a concept of weights. The neurons are of a binary nature. They fire if the sum of their inputs exceed a treshold...","categories": [],
+        "tags": [],
+        "url": "/2019/05/26/mcp_neurons.html",
+        "teaser": null
+      },{
+        "title": "The coin change problem",
+        "excerpt":"The change-making problem is a classic problem, where the goal is to divide an amount of money evenly into coins. We’ll use this problem to show off different programming paradigms/styles. We’ll imagine a vending machine that can take in coins until a treshold value (the price of the item a...","categories": [],
+        "tags": [],
+        "url": "/2019/06/23/coin-change.html",
+        "teaser": null
+      },{
+        "title": "Human Resource Machine *XASM*",
+        "excerpt":"Ok, so I came across a game called Human Resource Machine last week and blasted through it fairly quickly (I can only recommend it). The game basically is programming a sort of graphical assembly and the sole reason I’m writing about it here is: it supports importing and exporting and...","categories": [],
+        "tags": [],
+        "url": "/2019/10/06/hrm.html",
+        "teaser": null
+      },{
+        "title": "Post: Header Image with OpenGraph Override",
+        "excerpt":"This post has a header image with an OpenGraph override.   header:   image: /assets/images/banner.jpeg   og_image: /assets/images/banner.jpeg  ","categories": ["Layout","Uncategorized"],
+        "tags": ["edge case","image","layout"],
+        "url": "/layout/uncategorized/2021/07/27/test-page.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/07/27/welcome-to-jekyll.html",
+        "teaser": null
+      }]
