48ef9d5ffa530f6d9905e8a7f9f4248f88505527
SV-97 <volzstefan97@googlemail.com>
2021-08-01 13:34:29 +0200

    Added content

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,31 +1,25 @@
 var store = [{
         "title": "McCulloch/Pitts neuron model",
-        "excerpt":"So I came across the McCulloch/Pitts (I’ll just call it Mcp) neuron model while reading on ANNs today. The Mcp is a comparatively easy model that doesn’t have a concept of weights. The neurons are of a binary nature. They fire if the sum of their inputs exceed a treshold...","categories": [],
+        "excerpt":"So I came across the McCulloch/Pitts (I’ll just call it Mcp) neuron model while reading on ANNs today. The Mcp is a comparatively easy model that doesn’t have a concept of weights. The neurons are of a binary nature. They fire if the sum of their inputs exceed a treshold...","categories": ["soft-computing","math","CS","languages"],
         "tags": [],
-        "url": "/2019/05/26/mcp_neurons.html",
+        "url": "/soft-computing/math/cs/languages/mcp_neurons/",
         "teaser": null
       },{
         "title": "The coin change problem",
-        "excerpt":"The change-making problem is a classic problem, where the goal is to divide an amount of money evenly into coins. We’ll use this problem to show off different programming paradigms/styles. We’ll imagine a vending machine that can take in coins until a treshold value (the price of the item a...","categories": [],
+        "excerpt":"The change-making problem is a classic problem, where the goal is to divide an amount of money evenly into coins. We’ll use this problem to show off different programming paradigms/styles. We’ll imagine a vending machine that can take in coins until a treshold value (the price of the item a...","categories": ["math","programming","python","haskell"],
         "tags": [],
-        "url": "/2019/06/23/coin-change.html",
+        "url": "/math/programming/python/haskell/coin-change/",
         "teaser": null
       },{
-        "title": "Human Resource Machine *XASM*",
-        "excerpt":"Ok, so I came across a game called Human Resource Machine last week and blasted through it fairly quickly (I can only recommend it). The game basically is programming a sort of graphical assembly and the sole reason I’m writing about it here is: it supports importing and exporting and...","categories": [],
+        "title": "Human Resource Machine XASM",
+        "excerpt":"Ok, so I came across a game called Human Resource Machine last week and blasted through it fairly quickly (I can only recommend it). The game basically is programming a sort of graphical assembly and the sole reason I’m writing about it here is: it supports importing and exporting and...","categories": ["languages","assembly","rust","python"],
         "tags": [],
-        "url": "/2019/10/06/hrm.html",
+        "url": "/languages/assembly/rust/python/hrm/",
         "teaser": null
       },{
-        "title": "Post: Header Image with OpenGraph Override",
-        "excerpt":"This post has a header image with an OpenGraph override.   header:   image: /assets/images/banner.jpeg   og_image: /assets/images/banner.jpeg  ","categories": ["Layout","Uncategorized"],
-        "tags": ["edge case","image","layout"],
-        "url": "/layout/uncategorized/2021/07/27/test-page.html",
-        "teaser": null
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "title": "Generierende Funktionen und Integraltransformationen",
+        "excerpt":"Ich hatte schon länger eine Korrespondenz zwischen Differentialgleichungen und Rekurrenzgleichungen vermutet. Tatsächlich lassen sich die für Differentialgleichungen wichtigen Integraltransformationen als stetige generierende Funktionen auffassen. Diesen Zusammenhang erläutere ich hier näher.      View PDF      ","categories": ["math","generating-functions"],
         "tags": [],
-        "url": "/jekyll/update/2021/07/27/welcome-to-jekyll.html",
+        "url": "/math/generating-functions/generating-functions-integral-transforms/",
         "teaser": null
       }]
