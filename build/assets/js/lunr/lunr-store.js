de2de00d4ebc4e878be541f6eb52e734d5d393f6
SV-97 <volzstefan97@googlemail.com>
2021-12-08 23:50:02 +0100

    Updated Gemfile etc.

--- build/assets/js/lunr/lunr-store.js
+++ build/assets/js/lunr/lunr-store.js
@@ -5,12 +5,6 @@ var store = [{
         "url": "/soft-computing/math/cs/languages/mcp_neurons/",
         "teaser": null
       },{
-        "title": "The coin change problem",
-        "excerpt":"The change-making problem is a classic problem, where the goal is to divide an amount of money evenly into coins. We’ll use this problem to show off different programming paradigms/styles. We’ll imagine a vending machine that can take in coins until a treshold value (the price of the item a...","categories": ["math","programming","python","haskell"],
-        "tags": [],
-        "url": "/math/programming/python/haskell/coin-change/",
-        "teaser": null
-      },{
         "title": "Human Resource Machine XASM",
         "excerpt":"Ok, so I came across a game called Human Resource Machine last week and blasted through it fairly quickly (I can only recommend it). The game basically is programming a sort of graphical assembly and the sole reason I’m writing about it here is: it supports importing and exporting and...","categories": ["languages","assembly","rust","python"],
         "tags": [],
@@ -21,5 +15,23 @@ var store = [{
         "excerpt":"Ich hatte schon länger eine Korrespondenz zwischen Differentialgleichungen und Rekurrenzgleichungen vermutet. Tatsächlich lassen sich die für Differentialgleichungen wichtigen Integraltransformationen als stetige generierende Funktionen auffassen. Diesen Zusammenhang erläutere ich hier näher.      View PDF      ","categories": ["math","generating-functions"],
         "tags": [],
         "url": "/math/generating-functions/generating-functions-integral-transforms/",
+        "teaser": null
+      },{
+        "title": "Seminararbeit: Innere Geometrie der Flächen",
+        "excerpt":"Im Wintersemester 2021/22 hielt ich im Rahmen meines Studiums der Technomathematik einen Vortrag zum Thema Innere Geometrie der Flächen. Das zugehörige Skript kann hier und die Folien hier heruntergeladen werden.   Seminararbeit      View PDF       Folien      View PDF      ","categories": ["math","differential-geometry"],
+        "tags": [],
+        "url": "/seminar-innere-geometrie",
+        "teaser": null
+      },{
+        "title": "Rechtfertigung für Gramsche Determinante in Definition des k-Flächeninhalts",
+        "excerpt":"In der Definition des k-Volumens einer k-Fläche im R^n taucht die Gramsche Determinante der Parametrisierung auf. Wir wollen dies intuitiv begründen.      View PDF      ","categories": ["math","differential-geometry","analysis"],
+        "tags": [],
+        "url": "/math/differential-geometry/analysis/k-surfaces/",
+        "teaser": null
+      },{
+        "title": "Solving advent of code 2021's day 8 with linear algebra",
+        "excerpt":"Day 8 of Advent of code 2021 was a problem that became quite easily solveable by using linear algebra.      View PDF      ","categories": ["math","linear-algebra","aoc"],
+        "tags": [],
+        "url": "/math/linear-algebra/aoc/aoc21-8/",
         "teaser": null
       }]
