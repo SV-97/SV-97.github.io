36f3053c423b2dc2f804f6b6e8a6f49913559aae
SV-97 <SV-97@users.noreply.github.com>
2023-02-04 20:52:48 +0000

    jekyll build from Action 0442956d97806adf8ed0c34b28b3d5337c30f2a4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "McCulloch/Pitts neuron model",
+        "excerpt":"So I came across the McCulloch/Pitts (I’ll just call it Mcp) neuron model while reading on ANNs today. The Mcp is a comparatively easy model that doesn’t have a concept of weights. The neurons are of a binary nature. They fire if the sum of their inputs exceed a treshold...","categories": ["soft-computing","math","CS","languages"],
+        "tags": [],
+        "url": "/soft-computing/math/cs/languages/mcp_neurons/",
+        "teaser": null
+      },{
+        "title": "Human Resource Machine XASM",
+        "excerpt":"Ok, so I came across a game called Human Resource Machine last week and blasted through it fairly quickly (I can only recommend it). The game basically is programming a sort of graphical assembly and the sole reason I’m writing about it here is: it supports importing and exporting and...","categories": ["languages","assembly","rust","python"],
+        "tags": [],
+        "url": "/languages/assembly/rust/python/hrm/",
+        "teaser": null
+      },{
+        "title": "Generierende Funktionen und Integraltransformationen",
+        "excerpt":"Ich hatte schon länger eine Korrespondenz zwischen Differentialgleichungen und Rekurrenzgleichungen vermutet. Tatsächlich lassen sich die für Differentialgleichungen wichtigen Integraltransformationen als stetige generierende Funktionen auffassen. Diesen Zusammenhang erläutere ich hier näher.      View PDF      ","categories": ["math","generating-functions"],
+        "tags": [],
+        "url": "/math/generating-functions/generating-functions-integral-transforms/",
+        "teaser": null
+      },{
+        "title": "Seminararbeit: Innere Geometrie der Flächen",
+        "excerpt":"Im Wintersemester 2021/22 hielt ich im Rahmen meines Studiums der Technomathematik einen Vortrag zum Thema Innere Geometrie der Flächen. Das zugehörige Skript kann hier und die Folien hier heruntergeladen werden.   Seminararbeit      View PDF       Folien      View PDF      ","categories": ["math","differential-geometry"],
+        "tags": [],
+        "url": "/seminar-innere-geometrie",
+        "teaser": null
+      },{
+        "title": "Rechtfertigung für Gramsche Determinante in Definition des k-Flächeninhalts",
+        "excerpt":"In der Definition des k-Volumens einer k-Fläche im R^n taucht die Gramsche Determinante der Parametrisierung auf. Wir wollen dies intuitiv begründen.      View PDF      ","categories": ["math","differential-geometry","analysis"],
+        "tags": [],
+        "url": "/math/differential-geometry/analysis/k-surfaces/",
+        "teaser": null
+      },{
+        "title": "Solving advent of code 2021's day 8 with linear algebra",
+        "excerpt":"Day 8 of Advent of code 2021 was a problem that became quite easily solveable by using linear algebra.      View PDF      ","categories": ["math","linear-algebra","aoc"],
+        "tags": [],
+        "url": "/math/linear-algebra/aoc/aoc21-8/",
+        "teaser": null
+      }]
